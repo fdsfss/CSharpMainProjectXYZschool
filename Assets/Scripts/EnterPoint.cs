a106bf270a1eda4bc411798dc3cd42f396b08393
Ibragim Dana <d_ibragim@kbtu.kz>
2024-06-17 20:47:53 +0500

    first commit

--- Assets/Scripts/EnterPoint.cs
+++ Assets/Scripts/EnterPoint.cs
@@ -11,6 +11,7 @@ public class EnterPoint : MonoBehaviour
     [SerializeField] private Settings _settings;
     [SerializeField] private Canvas _targetCanvas;
     private float _timeScale = 1;
+    //comment 
     
     void Start()
     {
